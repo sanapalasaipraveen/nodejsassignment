8cb37ef40913f3fb0effee1a0b3255d6f69db0fc
sanapalasaipraveen <saipraveensanapala776@gmail.com>
2022-03-01 11:13:22 +0530

    done

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,24 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    return process.argv[5]
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require("readline");
+    const rl=readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    })
+    rl.question((ans)=>{
+        console.log(ans);
+        rl.close()
+    })
 }
 
 module.exports = {
